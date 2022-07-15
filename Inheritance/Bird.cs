1f6538bbaf5cbc0292b6fbd979cf9432bf25def1
Taylor Savage <taylorsavage93@gmail.com>
2022-07-15 11:36:33 -0400

    finished exercise 1

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    internal class Bird : Animal
+    {
+        public bool CanFly { get; set; }
+        public bool WebbedFeet { get; set; }
+        public string WingColor { get; set; }
+        public double BeakLength { get; set; }
+
+    }
+}
